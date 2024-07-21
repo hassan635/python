bca651eaed7baefa35d0845bc9c1f1c95c4509f1
hassan635 <hassan.a.tariq@gmail.com>
2024-07-21 13:40:19 +1000

    Ascii Art Generator

--- pythonTutorials/asciiArtGenerator/asciiArtGenerator.py
+++ pythonTutorials/asciiArtGenerator/asciiArtGenerator.py
@@ -1,28 +1,15 @@
 from ascii_magic import AsciiArt, Back, Front
 import glob
-
+import os
 
 if __name__ == "__main__":
-    
-    my_art = AsciiArt.from_image('./images/subscribe8.png')
-    my_output = my_art.to_ascii(columns=180, front=Front.GREEN, back=Back.BLACK)
-    print(my_output)
-    
-    #my_art = AsciiArt.from_image('./images/luffy.png')
-    #my_art.to_html_file('thumb.html', columns=200, width_ratio=2, additional_styles='color: green;')
-    
-    
-    #AsciiArt.to_html_file('./images/luffy.png', columns=200)
-    #AsciiArt.from_image("./images/subscribe4.png").to_terminal()
-    #AsciiArt.from_image("./images/luffyPoster.jpg").to_terminal()
-    
-    
-    """ images = []
-    for image in glob.glob("./images/*"):
-        images.append(image)
-    
-    for image in images:
-        my_art = AsciiArt.from_image(image)
-        my_art.to_terminal()
-        print("\n\n") """
+    '''
+    This scripts takes in an input image from the 
+    local directory and uses the Ascii Magic library
+    to output an Ascii art equivalent in an html file.
+    '''
+    input_image = AsciiArt.from_image('./images/shanks.jpeg')
+    output_file_name = 'art.html'
+    input_image.to_html_file(output_file_name, columns=200, width_ratio=2)
+    print(os.path.abspath(output_file_name))
 
